da24a0e0cdc09a0cc826bf5fea2b1f1c96ff9023
Johnny Wu <KarasAya@gmail.com>
2014-08-04 15:18:06 +0800

    simple server.js

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 var express = require('express');
 var app = express();
 
-// create an error with .status. we
-// can then use the property in our
-// custom error handler (Connect repects this prop as well)
+// create an error with .status.
 function error(status, msg) {
     var err = new Error(msg);
     err.status = status;
@@ -22,22 +20,14 @@ app.get(/^(.+)$/, function(req, res){
     res.sendfile( __dirname + req.params[0]); 
 });
 
-// middleware with an arity of 4 are considered
-// error handling middleware. When you next(err)
-// it will be passed through the defined middleware
-// in order, but ONLY those with an arity of 4, ignoring
-// regular middleware.
 app.use(function(err, req, res, next){
     // whatever you want here, feel free to populate
     // properties on `err` to treat it differently in here.
-    res.send(err.status || 500, { error: err.message });
+    res.status(err.status || 500).send({ error: err.message });
 });
 
-// our custom JSON 404 middleware. Since it's placed last
-// it will be the last middleware called, if all others
-// invoke next() and do not respond.
 app.use(function(req, res){
-    res.send(404, { error: "404 Error." });
+    res.status(404).send({ error: "404 Error." });
 });
 
 // start the server
